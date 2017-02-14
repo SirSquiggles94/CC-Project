e5695d35045b6e5fc19b2f8f63220df9539205cc
SirSquiggles94 <shaneking54@yahoo.co.uk>
2017-02-14 00:46:22 +0000

    Sign Up Page Update
    Have finished the signup page, added a bunch of javascript presence
    checking and setup the form to post to a process page. Invalid details
    error messages need to be added but this can only be done after the PHP
    is complete.

--- sign_up/index.php
+++ sign_up/index.php
@@ -18,7 +18,7 @@
 			<h2>Sign Up</h2>
 			
 			<div class = "form_panel">
-				<form>
+				<form id = "signup_form" method = "POST" action = "signup_process.php">
 				
 					<input name = "signup_choice" id = "signup_choice" type = "hidden" value = "">
 					
@@ -39,277 +39,143 @@
 					
 					</div>
 					
-					<div id = "part_2_restaraunt">
+					<div id = "part_2">
 					
-					<h6 id = "missing_info">Please enter missing information.</h6>
+						<h6 id = "missing_info">Please enter missing information.</h6>
 					
 						<div id = "col_1">
+						
+							<div id = "restaurant">
+								<label id = "restaurant_name_label" for = "business_name">Restaurant Name</label><br>
+							</div >
+							
+							<div id = "supplier">
+								<label id = "supplier_name_label" for = "business_name">Business Name</label><br>
+							</div>
+							<input id = "business_name" name = "business_name" type = "text" class = "form_field"><br>
 					
-							<label id = "restauraunt_name_label">Restaurant Name</label><br>
-							<input id = "restauraunt_name" name = "restauraunt_name" type = "text" class = "form_field"><br>
+							<label id = "forename_label" for = "forename">Owner/Manager Forename</label><br>
+							<input id = "forename" name = "forename" type = "text" class = "form_field"><br>
 							
-							<label id = "ofnl">Owner Forename</label><br>
-							<input id = "owner_forename" name = "owner_forename" type = "text" class = "form_field"><br>
+							<label id = "surname_label" for = "surname">Owner/Manager Surname</label><br>
+							<input id = "surname" name = "surname" type = "text" class = "form_field"><br>
+					
+							<label id = "email_label" for = "email">Email Address</label><br>
+							<input id = "email" name = "email_addr" type = "text" class = "form_field"><br>
 							
-							<label id = "osnl">Owner Surname</label><br>
-							<input id = "owner_surname" name = "owner_surname" type = "text" class = "form_field"><br>
-							
-							<label id = "email_label">Email Address</label><br>
-							<input id = "email" name = "email" type = "email" class = "form_field"><br>
-							
-							<label id = "tel_label">Telephone Number</label><br>
+							<label id = "tel_label" for = "tel_num">Telephone Number</label><br>
 							<input id = "tel_num" name = "telNum" type = "text" class = "form_field"><br>
-					
+							
 						</div>
-					
+						
 						<div id = "col_2">
 					
-							<label id = "str_name">Street Name</label><br>
+							<label id = "str_name" for = "street">Street Name</label><br>
 							<input id = "street" name = "street" type = "text" class = "form_field"><br>
 							
-							<label id = "city_label">Town/City</label><br>
+							<label id = "city_label" for = "city">Town/City</label><br>
 							<input id = "city" name = "city" type = "text" class = "form_field"><br>
 							
-							<label id = "county_label">County</label><br>
+							<label id = "county_label" for = "county">County</label><br>
 							<input id = "county" name = "county" type = "text" class = "form_field"><br>
 							
-							<label id = "post_label">Postcode</label><br>
+							<label id = "post_label" for = "postcode">Postcode</label><br>
 							<input id = "postcode" name = "postcode" type = "text" class = "form_field"><br>
 					
 					
 						</div>
 						
 						<input class = "choice_button" type = "button" id = "next" onClick = "part3();" value = "Next">
-				
 					</div>
 					
-					<div id = "part_2_supplier">
-					
-					<h6 id = "missing_info">Please enter missing information.</h6>
-					
-						<div id = "col_1">
-					
-							<label>Supplier Name</label><br>
-							<input name = "supplier_name" type = "text" class = "form_field"><br>
-							
-							<label>Manager Forename</label><br>
-							<input name = "manager_forename" type = "text" class = "form_field"><br>
-							
-							<label>Manager Surname</label><br>
-							<input name = "manager_surname" type = "text" class = "form_field"><br>
-							
-							<label>Email Address</label><br>
-							<input name = "email" type = "email" class = "form_field"><br>
-							
-							<label>Telephone Number</label><br>
-							<input name = "telNum" type = "text" class = "form_field"><br>
-					
-						</div>
-					
-						<div id = "col_2">
-					
-							<label>Unit Number</label><br>
-							<input name = "unit_num" type = "text" class = "form_field"><br>
-					
-							<label>Street Name</label><br>
-							<input name = "street" type = "text" class = "form_field"><br>
-							
-							<label>Town/City</label><br>
-							<input name = "city" type = "text" class = "form_field"><br>
-							
-							<label>County</label><br>
-							<input name = "county" type = "text" class = "form_field"><br>
-							
-							<label>Postcode</label><br>
-							<input name = "postcode" type = "text" class = "form_field"><br>
-					
-					
-						</div>
-						
-						<br>
-						
-						<input class = "choice_button" type = "button" id = "next" onClick = "part3();" value = "Next">
-				
-					</div>
 					
 					<div id = "part_3">
 					
-						Payment Screen
+						Choose payment signal.<br><br>
+					
+						<label id = "pay_success_label" for = "pay_success">Successful Payment:</label>
+						<input id = "pay_success" name =  "pay_success" type = "radio"><br><br>
+						
+						<label id = "pay_decline_label" for = "pay_decline">Payment Declined:</label>
+						<input id = "pay_decline" name = "pay_decline" type = "radio"><br><br>
+						
+						<label id = "incorrect_label" for = "incorect">Invalid Details:</label>
+						<input id = "incorrect" name = "incorrect" type = "radio"><br><br>
+						
+						<input class = "choice_button" id = "next" type = "submit" value = "Finish">
 						
 					</div>
 					
 					<script>
 					
+					
 						function setChoice(val)
 						{							
 							document.getElementById('signup_choice').value = val;
 							document.getElementById('part_1').style.display = "none";
-							
+							document.getElementById('part_2').style.display = "block";
 							
 							if(val == 1)
 							{
-								document.getElementById('part_2_restaraunt').style.display = "block";
+								document.getElementById("restaurant").style.display = "block";
 							}
 							else if(val == 2)
 							{
-								document.getElementById('part_2_supplier').style.display = "block";
+								document.getElementById("supplier").style.display = "block";
 							}
+							
+							
 						}
 						
 						function validate()
-						{
-							val = document.getElementById('signup_choice').value;
+						{							
+							var inputs = document.getElementsByTagName('input');
+							var labels = document.getElementsByTagName('label');
 							
+							var err = 0;
 							
-							if(val == 1)
+							for(count = 0; count < inputs.length - 8; count++)
 							{
-								var name = document.getElementById('restauraunt_name').value;
-								var O_Fname = document.getElementById('owner_forename').value;
-								var O_Sname = document.getElementById('owner_surname').value;
-								var email = document.getElementById('email').value;
-								var tel = document.getElementById('tel_num').value;
-								var AddrLn1 = document.getElementById('street').value;
-								var AddrLn2 = document.getElementById('city').value;
-								var AddrLn3 = document.getElementById('county').value;
-								var AddrLn4 = document.getElementById('postcode').value;
-								
-								var valState = 0;
-								
-								function show_err()
+								var input = inputs[count + 3].value;
+								var label = labels[count + 1];
+								if(input == "")
 								{
-									if(valState <= 0)
-									{
-										document.getElementById('missing_info').style.visibility = "hidden";
-									}
-									else
-									{
-										document.getElementById('missing_info').style.visibility = "visible";
-									}
+									err++;
+									label.style.color = "red";
+								}
+								else if(input != "")
+								{
+									label.style.color = "black";
+									err - 1;
 								}
 								
-								if(name == "")
-								{
-									document.getElementById('restauraunt_name_label').style.color = "red";
-									valState++;
-									show_err();
-								}
-								else
-								{
-									document.getElementById('restauraunt_name_label').style.color = "black";
-									valState - 1;
-									show_err();
-								}
-								
-								if(O_Fname == "")
-								{
-									document.getElementById('ofnl').style.color = "red";
-									valState++;	
-									show_err();																
-								}
-								else
-								{
-									document.getElementById('ofnl').style.color = "black";
-									valState - 1;
-									show_err();
-								}
-								
-								if(O_Sname == "")
-								{
-									document.getElementById('osnl').style.color = "red";
-									valState++;	
-									show_err();
-								}
-								else
-								{
-									document.getElementById('osnl').style.color = "black";
-									valState - 1;
-									show_err();
-								}
-								
-								if(email == "")
-								{
-									document.getElementById('email_label').style.color = "red";
-									valState++;
-									show_err();
-								}
-								else
-								{
-									document.getElementById('email_label').style.color = "black";
-									valState - 1;
-									show_err();
-								}
-								
-								if(tel == "")
-								{
-									document.getElementById('tel_label').style.color = "red";
-									valState++;	
-									show_err();
-								}
-								else
-								{
-									document.getElementById('tel_label').style.color = "black";
-									valState - 1;
-									show_err();
-								}
-								
-								if(AddrLn1 == "")
-								{
-									document.getElementById('str_name').style.color = "red";
-									valState++;	
-									show_err();
-								}
-								else
-								{
-									document.getElementById('str_name').style.color = "black";
-									valState - 1;
-									show_err();
-								}
-								
-								if(AddrLn2 == "")
-								{
-									document.getElementById('city_label').style.color = "red";
-									valState++;	
-									show_err();
-								}
-								else
-								{
-									document.getElementById('city_label').style.color = "black";
-									valState - 1;
-									show_err();
-								}
-								
-								if(AddrLn3 == "")
-								{
-									document.getElementById('county_label').style.color = "red";
-									valState++;	
-									show_err();
-								}
-								else
-								{
-									document.getElementById('county_label').style.color = "black";
-									valState - 1;
-									show_err();
-								}
-								
-								if(AddrLn4 == "")
-								{
-									document.getElementById('post_label').style.color = "red";
-									valState++;	
-									show_err();
-								}
-								else
-								{
-									document.getElementById('post_label').style.color = "black";
-									valState - 1;
-									show_err();
-								}
-								
-								alert(valState);
-								
-								if(valState == 0)
-								{
-									return true;
-								}
+							}
+							
+							input = inputs[3].value;
+							
+							
+							if(input == "")
+							{
+								label = labels[0];
+								label.style.color = "red";
+								label = labels[1];
+								label.style.color = "red";
+							}
+							else if(input != "")
+							{
+								label = labels[0];
+								label.style.color = "black";
+								label = labels[1];
+								label.style.color = "black";
+							}
+							
+							if(err != 0)
+							{
+								document.getElementById("missing_info").style.visibility = "visible";
+							}
+							else if(err == 0)
+							{
+								return true;
 							}
 						}
 					
@@ -317,8 +183,7 @@
 						{
 							if(validate() == true)
 							{							
-								document.getElementById('part_2_restaraunt').style.display = "none";
-								document.getElementById('part_2_supplier').style.display = "none";
+								document.getElementById('part_2').style.display = "none";
 								document.getElementById('part_3').style.display = "block";
 							}
 						}
