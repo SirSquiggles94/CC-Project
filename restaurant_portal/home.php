fa63c467059b3309799477a7078597cbe752b22c
SirSquiggles94 <shaneking54@yahoo.co.uk>
2017-02-19 02:12:46 +0000

    Initial Layout of 'Portal Page'
    Created the initial design for the portal page, need to add javascript
    and css media queries and decide how the menu will work.
    
    Have also edited the home page, will contain only php script for
    checking login and directing to the correct page. WIll contain nothing
    else.
    
    Moved login HTML to new file login.php

---
+++ restaurant_portal/home.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+	
+		<link rel = "stylesheet" href = "home_style.css" type = "text/css">
+		<link rel = "icon" type = "image/x-icon" href = "Images/fav.ico">
+		<title>The Chef's Pantry</title>
+	
+	
+	</head>
+	
+	<body>
+		
+		<header>
+		
+		
+			Header Text
+		
+		</header>
+		<nav>
+		
+		
+			Nav Text
+		
+		</nav>
+		<content>
+			
+			Content Text
+			
+		</content>
+	</body>
+</html>
--- restaurant_portal/index.php
+++ restaurant_portal/index.php
@@ -1,41 +1,6 @@
-<html>
-	<head>
-	
-	<?php
+<?php
 
-		include 'page_head.php';
-	?>
-	
-	
-	</head>
-	
-	<body>
-	
-		<content>
-	
-			<img id = "logo" src = "Images/logo.png">
-			<h2>Login</h2>
-			
-			<div class = "form_panel">
-	
-				<form id = "login_form" method = "POST" action = "login.php">
-				
-					<label id = "email_address_label" for = "email_address">Email Address: </label><br>
-					<input id = "email_address" name = "email_address" type = "text" class = "form_field"><br>
-					
-					<label id = "password_label" for = "password">Password: </label><br>
-					<input id = "password" name = "password" type = "password" class = "form_field">
-					
-					<hr class = "fancy_hr">
-					
-					<h5><a href = "login_help.php">Having trouble signing in?</a></h5>
-					
-					<input id = "next" class = "choice_button" type = "submit" value = "Login">
 
-				</form>
-				
-			</div>
+	//include 'lib.php';
 	
-		</content>
-	</body>
-</html>
+?>
