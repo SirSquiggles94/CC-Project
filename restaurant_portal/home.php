448665cf38a2d6ef09ff794f15db9f8a39d5685b
SirSquiggles94 <shaneking54@yahoo.co.uk>
2017-02-20 03:01:30 +0000

    Portal Page Update
    Have added javascript to navigation bar. hides when window is small and
    can be accessed through 'menu' button. javascript also prevents missing
    nav bar glitch.

--- restaurant_portal/home.php
+++ restaurant_portal/home.php
@@ -11,12 +11,71 @@
 	<body>
 		
 		<header>
+			<ul class = "title_menu">
+				
+				<li>
+				
+					<img id = "logo" src = "Images/logo.png">
+					
+				</li>
+				<li id = "small_menu" class = "small_menu">
+				
+
+					
+					<br>
+					<button id = "menu_button" onClick = "show_menu()">&#9776; Menu</button>
+					
+					<script>
+			
+						var click_count = 0;
 		
-		
-			Header Text
+						window.addEventListener("resize", enable_nav);
+
+						function enable_nav()
+						{
+							var win_size = window.innerWidth;
+							if(win_size > 775)
+							{
+								document.getElementById("nav_bar").style.visibility = "visible";
+								click_count = 1;
+							}
+							else if(win_size < 775)
+							{
+								document.getElementById("nav_bar").style.visibility = "hidden";
+								click_count = 0;
+							}
+						}
+						
+						function show_menu()
+						{	
+							if(click_count == 0)								
+							{
+								document.getElementById("nav_bar").style.visibility = "visible";
+								click_count = 1;
+							}
+							else if(click_count == 1)
+							{
+								document.getElementById("nav_bar").style.visibility = "hidden";
+								click_count = 0;
+							}
+						}
+						
+					</script>
+					
+				
+				</li>
+			
+			
+			
+			
+			
+			</ul>
+			
+			
+			
 		
 		</header>
-		<nav>
+		<nav id = "nav_bar">
 		
 		
 			Nav Text
